61f1178cf92549534818766b9409cbb868290289
passaeu.ti@gmail.com <passaeu.ti@gmail.com>
2019-08-25 23:35:12 -0300

    cm

---
+++ WebApi/Controllers/ClientesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebApi.Models;
+namespace WebApi.Controllers
+{
+
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+        public void Post(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+               clientes.Add(new Cliente(nome));
+            }
+        }
+
+    }
+}
